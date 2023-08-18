872742e856272e1d6cb636760652eeaa8f8948de
rumplestilzken <rumplestilzken@gmail.com>
2023-08-18 10:48:47 -0400

    Finished pixel5a implementation
    Change-Id: I9279bf790afbb440252ebd8f0f6d097c5ec92993

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,8 +2,10 @@ PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/lineage_privacysociety_pocket.mk \
 	$(LOCAL_DIR)/lineage_privacysociety_jelly2e.mk \
 	$(LOCAL_DIR)/lineage_privacysociety_atoml.mk \
+	$(LOCAL_DIR)/lineage_privacysociety_pixel5a.mk \
 
 COMMON_LUNCH_CHOICES := \
 	lineage_privacysociety_pocket-userdebug \
 	lineage_privacysociety_jelly2e-userdebug \
 	lineage_privacysociety_atoml-userdebug \
+	lineage_privacysociety_pixel5a-userdebug \
--- lineage_privacysociety_pixel5a.mk
+++ lineage_privacysociety_pixel5a.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, device/privacysociety/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := privacysociety_jelly2e
-PRODUCT_BRAND := Unihertz
-PRODUCT_SYSTEM_BRAND := Unihertz
-PRODUCT_MANUFACTURER := Unihertz
-PRODUCT_NAME := Jelly_2E
-PRODUCT_MODEL := Jelly 2E
+PRODUCT_DEVICE := privacysociety_pixel5a
+PRODUCT_BRAND := Google
+PRODUCT_SYSTEM_BRAND := Google
+PRODUCT_MANUFACTURER := Google
+PRODUCT_NAME := Pixel_5a
+PRODUCT_MODEL := Pixel 5a
 
-RESOURCE_PATH := device/privacysociety/privacysociety_jelly2e
+RESOURCE_PATH := device/privacysociety/privacysociety_pixel5a
 
 #Init Scripts
 PRODUCT_COPY_FILES += \
