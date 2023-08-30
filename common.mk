08221383e7f49c567ce16036245cf28d4247603c
rumplestilzken <rumplestilzken@gmail.com>
2023-08-29 20:54:34 -0400

    Added wallpaper
    Change-Id: I7c75d5d93c35992748ad0dd7c7ebb760381317f1

--- common.mk
+++ common.mk
@@ -7,6 +7,8 @@ TARGET_NO_KERNEL_OVERRIDE := true
 #Init Scripts
 PRODUCT_COPY_FILES += \
 	$(RESOURCE_PATH)/init/privacysociety_firstboot.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/privacysociety_firstboot.rc \
+	$(RESOURCE_PATH)/sysconfig:(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig \
+	$(RESOURCE_PATH)/wallpaper.png:(TARGET_COPY_OUT_SYSTEM)/product/media/wallpaper.png \
 
 #Allow resigned images.
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
